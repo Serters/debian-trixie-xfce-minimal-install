8c9bdb27603f934895ef79cc6cb5212f8c22b693
serters <strahinja.antic002@gmail.com>
2025-10-31 22:18:49 +0000

    ⚙️

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@
 # --------------------->  START OF SCRIPT <-----------------------
 # ================================================================
 
+# https://github.com/Serters/debian-trixie-xfce-minimal-install
+
 # ===============================
 #        Confirmation
 # ===============================
@@ -24,15 +26,16 @@ sudo -v
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 mkdir -p assets
-mkdir -p startup
 mkdir -p backup
+mkdir -p logs
+mkdir -p startup
 
-LOG_FILE="${SCRIPT_DIR}/setup.log"
+LOG_FILE="${SCRIPT_DIR}/logs/setup.log"
 touch "$LOG_FILE"
 exec > >(tee -a "$LOG_FILE") 2>&1
 echo "start: $(date)"
 
-PROGRESS_FILE="${SCRIPT_DIR}/progress.txt"
+PROGRESS_FILE="${SCRIPT_DIR}/logs/progress.txt"
 touch "$PROGRESS_FILE"
 
 # ================================================================
@@ -44,27 +47,17 @@ touch "$PROGRESS_FILE"
 # ===============================
 
 upgrade_packages() {
-    echo "upgrade_packages START" > "$PROGRESS_FILE"
-
-    sudo tee /etc/apt/sources.list >/dev/null <<'EOF'
-deb https://deb.debian.org/debian trixie main contrib non-free non-free-firmware
-# deb-src https://deb.debian.org/debian trixie main contrib non-free non-free-firmware
-
-deb https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
-# deb-src https://security.debian.org/debian-security trixie-security main contrib non-free non-free-firmware
-
-deb https://deb.debian.org/debian trixie-updates main contrib non-free non-free-firmware
-# deb-src https://deb.debian.org/debian trixie-updates main contrib non-free non-free-firmware
-EOF
-
+	
+	echo "upgrade_packages START" > "$PROGRESS_FILE"
+	
     sudo apt update
-    sudo apt upgrade
-    sudo apt autopurge
-    sudo apt autoclean
+    sudo apt upgrade -y
+    sudo apt autopurge -y
+    sudo apt autoclean -y
 
     echo "upgrade_packages END" > "$PROGRESS_FILE"
 }
-# To run upgrade_packages, uncomment the following line:
+# To upgrade packages, uncomment the following line:
 # upgrade_packages
 
 # ===============================
@@ -73,12 +66,12 @@ EOF
 install_minimal_xfce() {
     echo "install_minimal_xfce START" > "$PROGRESS_FILE"
 
-    sudo apt install libxfce4ui-utils thunar xfce4-panel
-    sudo apt install --no-install-recommends xfce4-session xfwm4 xfce4-terminal
+    sudo apt install -y libxfce4ui-utils thunar xfce4-panel
+    sudo apt install -y --no-install-recommends xfce4-session xfwm4 xfce4-terminal
 
     echo "install_minimal_xfce END" > "$PROGRESS_FILE"
 }
-# To run install_minimal_xfce, uncomment the following line:
+# To install minimal xfce desktop environment, uncomment the following line:
 # install_minimal_xfce
 
 # ===============================
@@ -87,8 +80,8 @@ install_minimal_xfce() {
 install_xorg() {
     echo "install_xorg START" > "$PROGRESS_FILE"
 
-    sudo apt install xserver-xorg
-    sudo apt install --no-install-recommends xinit
+    sudo apt install -y xserver-xorg
+    sudo apt install -y --no-install-recommends xinit
 
     cat <<'EOF' >>~/.profile
 # runs startx when logged into the TTY1.
@@ -100,16 +93,46 @@ EOF
 
 echo "install_xorg END" > "$PROGRESS_FILE"
 }
-# To run install_xorg, uncomment the following line:
+# To install xorg, uncomment the following line:
 # install_xorg
+
+# ===============================
+#        Minimal Standard Utils
+# ===============================
+
+# Customize by deleting lines in clean_packages as needed:
+install_minimal_standard_system_utils() {
+    sudo apt install -y bash-completion \
+    bzip2 \
+    dbus \
+    file \
+    liblockfile-bin \
+    libnss-systemd \
+    lsof \
+    media-types \
+    netcat-traditional \
+    pciutils \
+    ucf \
+    usbutils \
+    xz-utils
+}
+# To install minimal standard system utils, uncomment the following line:
+# install_minimal_standard_system_utils
+
+install_man() {
+    sudo apt install -y man-db \
+    manpages 
+}
+# To install manpages and man-db, uncomment the following line:
+# install_man
 
 # ===============================
 #        Package Cleanup
 # ===============================
 
 # Customize by deleting lines in clean_packages as needed:
-clean_packages() {
-    echo "clean_packages START" > "$PROGRESS_FILE"
+remove_packages() {
+    echo "remove_packages START" > "$PROGRESS_FILE"
 
     sudo apt purge apt-listchanges \
         bind9-dnsutils \
@@ -138,14 +161,25 @@ clean_packages() {
     sudo apt autopurge
     sudo apt autoclean
 
-    echo "clean_packages END" > "$PROGRESS_FILE"
+    echo "remove_packages END" > "$PROGRESS_FILE"
 }
 # To remove unnecessary packages, uncomment the following line:
-# clean_packages
+# remove_packages
 
 # ================================================================
 # --------------------->  END: Minimal Setup  <-------------------
 # ================================================================
+
+minimal_setup() {
+	upgrade_packages
+	install_minimal_xfce
+	install_xorg
+	install_minimal_standard_system_utils
+	install_man
+	remove_packages
+}
+# To run everything from the minimal setup, uncomment the following line:
+# minimal_setup
 
 # ================================================================
 # --------------------->  START: Utils Setup  <-------------------
@@ -160,32 +194,38 @@ install_extra_tools() {
     echo "install_extra_tools START" > "$PROGRESS_FILE"
 
     # firewall, partition tools & graphical package manager
-    sudo apt install ufw && sudo ufw enable
-    sudo apt install gparted synaptic
+    sudo apt install -y ufw && sudo ufw enable
+    sudo apt install -y gparted synaptic
 
     # resource monitors
-    sudo apt install htop btop fastfetch
+    sudo apt install -y htop btop fastfetch
 
     # fonts
-    sudo apt install --no-install-recommends fonts-noto-core fonts-noto-color-emoji
+    sudo apt install -y --no-install-recommends fonts-noto-core fonts-noto-color-emoji
 
     # kitty terminal
-    # sudo apt install --no-install-recommends kitty
+    sudo apt install -y --no-install-recommends kitty
 
-    # media-player, image-viewer & text editor
-    sudo apt install --no-install-recommends mpv ristretto geany
+    # calculator, media-player, image-viewer, text editor & video thumbanils
+    sudo apt install -y --no-install-recommends galculator mpv ristretto geany ffmpegthumbnailer
 
-    # whisker menu, screenshot, clipboard history, wallpaper tool & video thumbanils
-    sudo apt install --no-install-recommends xfce4-screenshooter xfce4-clipman xfce4-whiskermenu-plugin xwallpaper ffmpegthumbnailer
+    # whisker menu, screenshot, clipboard history & wallpaper tool 
+    sudo apt install -y --no-install-recommends xfce4-whiskermenu-plugin xfce4-screenshooter xfce4-clipman xwallpaper 
 
     # cli tools
-    sudo apt install curl git make cryptsetup
+    sudo apt install -y curl wget git make p7zip-full cryptsetup 
 
     # brave browser
     sudo curl -fsS https://dl.brave.com/install.sh | sh
 
     # firefox browser
-    # sudo apt install firefox-esr
+    # sudo apt install -y firefox-esr
+
+    # SSH
+    # sudo apt install -y openssh-client openssh-server && sudo ufw allow ssh
+
+    # other
+    # sudo apt install -y vim tmux bc lm-sensors
 
     echo "install_extra_tools END" > "$PROGRESS_FILE"
 }
@@ -196,12 +236,13 @@ install_extra_tools() {
 #        System Utilities
 # ===============================
 
+# network managment
 install_connman() {
     echo "install_connman START" > "$PROGRESS_FILE"
 
-    sudo apt install --no-install-recommends connman-gtk
-    sudo apt install wpasupplicant
-    # sudo apt install bluez
+    sudo apt install -y --no-install-recommends connman-gtk
+    sudo apt install -y wpasupplicant
+    sudo apt install -y bluez
 
     mkdir -p ~/.config/autostart
     cat >~/.config/autostart/connman-gtk.desktop <<EOF
@@ -220,6 +261,7 @@ Hidden=false
 EOF
 
     sudo systemctl disable wpa_supplicant
+    sudo systemctl mask dundee
     sudo cp /etc/network/interfaces backup/interfaces.bak
 
     sudo tee /etc/network/interfaces >/dev/null <<EOF
@@ -236,10 +278,11 @@ EOF
     echo "install_connman END" > "$PROGRESS_FILE"
 }
 
+# power and battery information
 install_cbatticon() {
     echo "install_cbatticon START" > "$PROGRESS_FILE"
 
-    sudo apt install cbatticon
+    sudo apt install -y cbatticon light
 
     mkdir -p ~/.config/autostart
     cat >~/.config/autostart/cbatticon.desktop <<EOF
@@ -260,10 +303,11 @@ EOF
 
 }
 
+# basic volume control
 install_volumeicon() {
     echo "install_volumeicon START" > "$PROGRESS_FILE"
 
-    sudo apt install volumeicon-alsa
+    sudo apt install -y volumeicon-alsa
 
     mkdir -p ~/.config/autostart
     cat >~/.config/autostart/volumeicon.desktop <<EOF
@@ -284,10 +328,15 @@ EOF
     echo "install_volumeicon END" > "$PROGRESS_FILE"
 }
 
-# To install system trey tools, uncomment the lines you need:
+# To install lighter system trey tools, uncomment the lines you need:
 # install_connman
 # install_cbatticon
 # install_volumeicon
+
+# To install system trey tools (these have more features but use more resources), uncomment the lines you need:
+# sudo apt install -y network-manager
+# sudo apt install -y xfce4-power-manager
+# sudo apt install -y xfce4-pulseaudio-plugin pulseaudio-module-bluetooth
 
 # ================================================================
 # --------------------->  END: Utils Setup  <---------------------
@@ -300,21 +349,27 @@ EOF
 # ===============================
 #        /tmp partition
 # ===============================
-# To change the size of /tmp partition to 2GiB, uncomment the following line:
-#sudo sed -i 's/size=50%%/size=2G/' /usr/lib/systemd/system/tmp.mount
+# To change the size of /tmp partition to 2GiB (default value is 50% of your ram), uncomment the following line:
+# sudo sed -i 's/size=50%%/size=2G/' /usr/lib/systemd/system/tmp.mount
 
 move_brave_cache() {
-    cd ~/.cache
-    rm -rf BraveSoftware
-    ln -s /tmp BraveSoftware
+	echo "move_brave_cache START" > "$PROGRESS_FILE"
+	
+    rm -rf "$HOME/.cache/BraveSoftware"
+    ln -s /tmp "$HOME/.cache/BraveSoftware"
+    
+    echo "move_brave_cache END" > "$PROGRESS_FILE"
 }
 # To move brave cache to ram, uncomment the following line:
 # move_brave_cache
 
 move_thumbnails_cache() {
-    cd ~/.cache
-    rm -rf thumbnails
-    ln -s /tmp thumbnails
+	echo "move_thumbnails_cache START" > "$PROGRESS_FILE"
+	
+    rm -rf "$HOME/.cache/thumbnails"
+    ln -s /tmp "$HOME/.cache/thumbnails"
+    
+    echo "move_thumbnails_cache END" > "$PROGRESS_FILE"
 }
 # To move thumbnails cache to ram, uncomment the following line:
 # move_thumbnails_cache
@@ -323,70 +378,39 @@ move_thumbnails_cache() {
 #        Disable Modules
 # ===============================
 
-blacklist_modules() {
-    # Blacklist Bluetooth
-    echo "blacklist btusb" | sudo tee /etc/modprobe.d/blacklist-bluetooth.conf
-
-    # Blacklist Webcam
+disable_webcam() {
+	echo "disable_webcam START" > "$PROGRESS_FILE"
+	
     echo "blacklist uvcvideo" | sudo tee /etc/modprobe.d/blacklist-webcam.conf
 
     cat >>~/.bash_aliases <<EOF
-alias bluetooth-start="sudo modprobe -v btusb"
 alias webcam-start="sudo modprobe -v uvcvideo"
-alias bluetooth-stop="sudo modprobe -r btusb"
 alias webcam-stop="sudo modprobe -r uvcvideo"
 EOF
+
+	echo "disable_webcam END" > "$PROGRESS_FILE"
 }
-# To disable bluetooth and webcam modules, uncomment the following line:
-# blacklist_modules
+# To disable the webcam module uncomment the following line:
+# disable_webcam
 
 # ===============================
 #        Disable Services
 # ===============================
-#sudo systemctl disable bluetooth
 
-sudo systemctl mask ofono
-sudo systemctl mask dundee
-
-#sudo systemctl disable apt-daily-upgrade.timer
-#sudo systemctl disable apt-daily.timer
+disable_autoupdate() {
+	sudo systemctl disable apt-daily-upgrade.timer
+	sudo systemctl disable apt-daily.timer
+}
+# To disable automatic updates, uncomment the following line:
+# disable_autoupdate
 
 # ===============================
 #        Startup scripts
 # ===============================
-mute_on_startup() {
-    mkdir -p ~/.config/autostart
-
-    cat <<'EOF' >"$SCRIPT_DIR/startup/mute_volume.sh"
-# Mute the Master channel
-amixer sset Master mute
-
-# Set the Master channel volume to 0%
-amixer sset Master 0%
-EOF
-    chmod +x "$SCRIPT_DIR/startup/mute_volume.sh"
-
-    cat >~/.config/autostart/volume.desktop <<EOF
-[Desktop Entry]
-Encoding=UTF-8
-Version=0.9.4
-Type=Application
-Name=volume on boot
-Comment=
-Exec=${SCRIPT_DIR}/startup/mute_volume.sh
-OnlyShowIn=XFCE;
-RunHook=0
-StartupNotify=false
-Terminal=false
-Hidden=false
-EOF   
-}
-# To mute volume on startup, uncomment the following line:
-# mute_on_startup
 
 set_xwallpaper() {
     mkdir -p "$HOME/.config/autostart"
-    echo "xwallpaper --center \"$SCRIPT_DIR/assets/wallpaper.png\"" >"$SCRIPT_DIR/startup/wallpaper.sh"
+    echo "xwallpaper --zoom \"$SCRIPT_DIR/assets/wallpaper.png\"" >"$SCRIPT_DIR/startup/wallpaper.sh"
     chmod +x "$SCRIPT_DIR/startup/wallpaper.sh"
 
     cat <<EOF >"$HOME/.config/autostart/wallpaper.desktop"
@@ -403,6 +427,8 @@ StartupNotify=false
 Terminal=false
 Hidden=false
 EOF
+
+xwallpaper --zoom "$SCRIPT_DIR/assets/wallpaper.png"
 }
 # To set a wallpaper using xwallpaper, uncomment the following line:
 # set_xwallpaper
@@ -415,7 +441,7 @@ pkill xfconfd
 enable_numpad() {
     mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
-    cp -fv /assets/keyboards.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+    cp -fv "$SCRIPT_DIR/assets/keyboards.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/"
 }
 # To enable numpad on boot, uncomment the following line:
 # enable_numpad
@@ -423,7 +449,7 @@ enable_numpad() {
 configure_shortcuts() {
     mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
-    cp -fv /assets/xfce4-keyboard-shortcuts.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+    cp -fv "$SCRIPT_DIR/assets/xfce4-keyboard-shortcuts.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/"
 }
 # To enable whisker menu on super key, uncomment the following line:
 # configure_shortcuts
@@ -431,7 +457,7 @@ configure_shortcuts() {
 configure_xfwm() {
     mkdir -p ~/.config/xfce4/xfconf/xfce-perchannel-xml/
 
-    cp -fv /assets/xfwm4.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/
+    cp -fv "$SCRIPT_DIR/assets/xfwm4.xml" "$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/"
 }
 # To configure xfwm, uncomment the following line:
 # configure_xfwm
@@ -439,6 +465,7 @@ configure_xfwm() {
 # ================================================================
 # --------------------->  END: Tweaks Setup  <--------------------
 # ================================================================
+
 echo "end: $(date)"
 # ================================================================
 # --------------------->  END OF SCRIPT <-------------------------
